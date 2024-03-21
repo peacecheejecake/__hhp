97df2a024178db242647d3055c4cafe63c24d185
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-22 07:54:31 +0900

    2b: Pass 3 test cases

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,40 @@
 export function createHooks(callback) {
+  const _states = [];
+  let _stateIndex = 0;
+
+  const _callback = () => {
+    _stateIndex = 0;
+    callback();
+  };
+
+  const _createState = (initState) => {
+    let state = initState;
+    const setState = (newState) => {
+      if (state !== newState) {
+        state = newState;
+        _callback();
+      }
+    };
+
+    return [state, setState];
+  };
+
   const useState = (initState) => {
-    return [];
+    if (_states.length <= _stateIndex) {
+      _states.push(_createState(initState));
+    }
+
+    const state = _states[_stateIndex];
+    _stateIndex += 1;
+
+    return state;
   };
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
-  const resetContext = () => {
-
-  }
+  const resetContext = () => {};
 
   return { useState, useMemo, resetContext };
 }
