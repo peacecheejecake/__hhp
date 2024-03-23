54b5937db02cb0a8e586e1878c83fb934c1a25c2
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-23 14:57:36 +0900

    Solve 2b

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -2,26 +2,28 @@ export function createHooks(callback) {
   const _states = [];
   let _stateIndex = 0;
 
-  const _callback = () => {
-    _stateIndex = 0;
-    callback();
-  };
+  const _memos = [];
+  let _memoIndex = 0;
 
   const _createState = (initState) => {
-    let state = initState;
+    const index = _states.length;
     const setState = (newState) => {
+      const state = _states[index][0];
       if (state !== newState) {
-        state = newState;
-        _callback();
+        _states[index][0] = newState;
+        callback();
       }
     };
+    _states.push([initState, setState]);
+  };
 
-    return [state, setState];
+  const _createMemo = (fn, refs) => {
+    _memos.push([fn(), [...refs]]);
   };
 
   const useState = (initState) => {
     if (_states.length <= _stateIndex) {
-      _states.push(_createState(initState));
+      _createState(initState);
     }
 
     const state = _states[_stateIndex];
@@ -31,10 +33,27 @@ export function createHooks(callback) {
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    if (_memos.length <= _memoIndex) {
+      _createMemo(fn, refs);
+    }
+
+    const [prevValue, prevRefs] = _memos[_memoIndex];
+    let value = prevValue;
+    let isRefChanged = refs.some((ref, index) => ref !== prevRefs[index]);
+    if (isRefChanged) {
+      value = fn();
+    }
+
+    _memos[_memoIndex] = [value, refs];
+    _memoIndex += 1;
+
+    return value;
   };
 
-  const resetContext = () => {};
+  const resetContext = () => {
+    _stateIndex = 0;
+    _memoIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
